9a23305dbf9a6204de9bcf0b9b572494e9b2ec22
hyunsoo922 <138758289+hyunsoo922@users.noreply.github.com>
2024-05-20 16:28:42 +0900

    Delete ProgrammingTask01 directory

--- ProgrammingTask01/AbstractTest.java
+++
@@ -1,8 +0,0 @@
-package chap07.ProgrammingTask01;
-
-public class AbstractTest {
-    public static void main(String[] args) {
-        Concrete c = new Concrete(100,50);
-        c.show();
-    }
-}
